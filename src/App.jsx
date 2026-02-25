7de20e293a09b75dc4c8cfda568acdc16c718ee8
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-02-25 11:24:28 -0500

    snap_section_bs

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { Header } from "./components/Header"
+import { Home } from "./pages/Home"
+
+
+export const App = () => {
+  return (
+    <>
+
+    <Header/>
+    <Home/>
+    
+    
+    
+    
+    </>
+  )
+}
+
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,41 @@
+
+export const Home = () => {
+    return (
+        <>
+
+            <div className="container text-center">
+                <h1>Row1</h1>
+                <div className="row">
+                    <div className="col-6 col-md-4 col-lg-3 line" >
+                        Column
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 line">
+                        Column
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 line">
+                        Column
+                    </div>
+                    <div className="col-6 col-md-4 col-lg-3 line">
+                        Column
+                    </div>
+                </div>
+                <h1>Row2</h1>
+                <div className="row">
+                    <div className="col-6 line">
+                        Column
+                    </div>
+                    <div className="col-6 line">
+                        Column
+                    </div>
+                    <div className="col-6 line">
+                        Column
+                    </div>
+                    <div className="col-6 line">
+                        Column
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
