ebc0e441e248a5e9ea6f9a76f79008ea78ab7614
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-02-26 10:44:23 -0500

    szs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,37 +1,32 @@
+import "../stylesheets/Home.css"
 
 export const Home = () => {
+
+
+
     return (
         <>
 
-            <div className="container text-center">
-                <h1>Row1</h1>
+            <div className="container">
                 <div className="row">
-                    <div className="col-6 col-md-4 col-lg-3 line" >
-                        Column
+                    <div className="col-6">
+                        <div>
+                            <h1>Make <br /> remote work</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, reprehenderit ratione culpa numquam rem, expedita alias, dolorum suscipit repudiandae dignissimos error voluptatibus. Explicabo pariatur sunt ipsam. Culpa rem officiis omnis.</p>
+                            <a href="">Learn more</a>
+
+                            <div>
+                                <img src="" alt="" />
+                                <img src="" alt="" />
+                                <img src="" alt="" />
+                                <img src="" alt="" />
+                            </div>
+                        </div>
                     </div>
-                    <div className="col-6 col-md-4 col-lg-3 line">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3 line">
-                        Column
-                    </div>
-                    <div className="col-6 col-md-4 col-lg-3 line">
-                        Column
-                    </div>
-                </div>
-                <h1>Row2</h1>
-                <div className="row">
-                    <div className="col-6 line">
-                        Column
-                    </div>
-                    <div className="col-6 line">
-                        Column
-                    </div>
-                    <div className="col-6 line">
-                        Column
-                    </div>
-                    <div className="col-6 line">
-                        Column
+                    <div className="col-6">
+                        <div>
+                            <img className="img-fluid" src="./o.jpg" alt="" />
+                        </div>
                     </div>
                 </div>
             </div>
