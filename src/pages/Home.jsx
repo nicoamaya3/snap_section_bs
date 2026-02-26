a4d53260d48f9776b0dc0c0ae76e50ffde068a7b
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-02-26 11:22:18 -0500

    AhoraSi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,19 +10,19 @@ export const Home = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-6">
-                        <div className="d-flex flex-column justify-content-center h-100 ">
+                        <div className="d-flex flex-column justify-content-center h-100 gap-5 ">
 
                             <div>
                                 <h1>Make <br /> remote work</h1>
                                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, reprehenderit ratione culpa numquam rem, expedita alias, dolorum suscipit repudiandae dignissimos error voluptatibus. Explicabo pariatur sunt ipsam. Culpa rem officiis omnis.</p>
-                                <a href="">Learn more</a>
+                                <a href="http://www.jango.com" className="btn btn-dark" >Learn more</a>
+                            </div>
 
-                                <div className="d-flex align-items-center gap-5 ">
-                                    <img src="./linear.svg" alt="" height="25" />
-                                    <img src="./shopify.svg" alt="" height="25" />
-                                    <img src="./stripe.svg" alt="" height="25" />
-                                    <img src="./wiz.svg" alt="" height="25" />
-                                </div>
+                            <div className="d-flex align-items-center gap-5 ">
+                                <img src="./linear.svg" alt="" height="25" />
+                                <img src="./shopify.svg" alt="" height="25" />
+                                <img src="./stripe.svg" alt="" height="25" />
+                                <img src="./wiz.svg" alt="" height="25" />
                             </div>
                         </div>
                     </div>
