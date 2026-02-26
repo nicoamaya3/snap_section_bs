c445a0805ae569de354542a7b4b5f4151d0e75a7
nicoamaya3 <nicolasatehortuaamaya@gmail.com>
2026-02-26 11:11:21 -0500

    ya

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,21 +10,25 @@ export const Home = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-6">
-                        <div>
-                            <h1>Make <br /> remote work</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, reprehenderit ratione culpa numquam rem, expedita alias, dolorum suscipit repudiandae dignissimos error voluptatibus. Explicabo pariatur sunt ipsam. Culpa rem officiis omnis.</p>
-                            <a href="">Learn more</a>
+                        <div className="d-flex flex-column justify-content-center h-100 ">
 
                             <div>
-                                <img src="" alt="" />
-                                <img src="" alt="" />
-                                <img src="" alt="" />
-                                <img src="" alt="" />
+                                <h1>Make <br /> remote work</h1>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, reprehenderit ratione culpa numquam rem, expedita alias, dolorum suscipit repudiandae dignissimos error voluptatibus. Explicabo pariatur sunt ipsam. Culpa rem officiis omnis.</p>
+                                <a href="">Learn more</a>
+
+                                <div className="d-flex align-items-center gap-5 ">
+                                    <img src="./linear.svg" alt="" height="25" />
+                                    <img src="./shopify.svg" alt="" height="25" />
+                                    <img src="./stripe.svg" alt="" height="25" />
+                                    <img src="./wiz.svg" alt="" height="25" />
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="col-6">
                         <div>
+                            <div className=""></div>
                             <img className="img-fluid" src="./o.jpg" alt="" />
                         </div>
                     </div>
